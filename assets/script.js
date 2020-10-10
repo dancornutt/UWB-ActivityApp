e0b619f2ddcb40af20068430633538de1f324bf0
Dan Cornutt <dacornutt@gmail.com>
2020-10-10 13:39:26 -0700

    Added function for geolocation of client

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,21 @@
-//This is new code I did (DAN)
+//From our class activities
+function getLocation() {
+    // Make sure browser supports this feature
+    if (navigator.geolocation) {
+      // Provide our showPosition() function to getCurrentPosition
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } 
+    else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  // This will get called after getCurrentPosition()
+  function showPosition(position) {
+    // Grab coordinates from the given object
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    console.log("Your coordinates are Latitude: " + lat + " Longitude " + lon);
+  }
+
+getLocation();
